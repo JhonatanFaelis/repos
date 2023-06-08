74a31652041202547700c64405de1309561f51d7
BrunoBarrichello <bruno.barrichello@hotmail.com>
2023-06-08 13:45:50 -0300

    08/06/23 Bruno 13:45

--- LR_Financ/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LR_Financ.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Site_LR/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Site_LR.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
